7791dcab5424b41ec0b83e22c529c4f76a7beaf9
EmilioMat <emiliomartinn09@gmail.com>
2025-06-01 11:03:55 +0200

    Configuraciones finales del panel de administración

--- resources/js/Pages/Admin/Components/Sidebar.vue
+++ resources/js/Pages/Admin/Components/Sidebar.vue
@@ -126,6 +126,7 @@ import { Link } from "@inertiajs/vue3";
         >
             <a
                 href="#"
+                data-tooltip-target="tooltip-under-construction-1"
                 class="inline-flex justify-center p-2 text-gray-500 rounded cursor-pointer dark:text-gray-400 hover:text-gray-900 dark:hover:text-white hover:bg-gray-100 dark:hover:bg-gray-600"
             >
                 <svg
@@ -140,9 +141,17 @@ import { Link } from "@inertiajs/vue3";
                     ></path>
                 </svg>
             </a>
+            <div
+                id="tooltip-under-construction-1"
+                role="tooltip"
+                class="inline-block absolute invisible z-10 py-2 px-3 text-sm font-medium text-white bg-gray-900 rounded-lg shadow-sm opacity-0 transition-opacity duration-300 tooltip"
+            >
+                Página en construcción
+                <div class="tooltip-arrow" data-popper-arrow></div>
+            </div>
             <a
                 href="#"
-                data-tooltip-target="tooltip-settings"
+                data-tooltip-target="tooltip-under-construction-2"
                 class="inline-flex justify-center p-2 text-gray-500 rounded cursor-pointer dark:text-gray-400 dark:hover:text-white hover:text-gray-900 hover:bg-gray-100 dark:hover:bg-gray-600"
             >
                 <svg
@@ -160,11 +169,11 @@ import { Link } from "@inertiajs/vue3";
                 </svg>
             </a>
             <div
-                id="tooltip-settings"
+                id="tooltip-under-construction-2"
                 role="tooltip"
                 class="inline-block absolute invisible z-10 py-2 px-3 text-sm font-medium text-white bg-gray-900 rounded-lg shadow-sm opacity-0 transition-opacity duration-300 tooltip"
             >
-                Settings page
+                Página en construcción
                 <div class="tooltip-arrow" data-popper-arrow></div>
             </div>
             <button
@@ -176,39 +185,10 @@ import { Link } from "@inertiajs/vue3";
                     aria-hidden="true"
                     class="h-5 w-5 rounded-full mt-0.5"
                     xmlns="http://www.w3.org/2000/svg"
-                    xmlns:xlink="http://www.w3.org/1999/xlink"
-                    viewBox="0 0 3900 3900"
+                    viewBox="0 0 512 512"
                 >
-                    <path fill="#b22234" d="M0 0h7410v3900H0z" />
-                    <path
-                        d="M0 450h7410m0 600H0m0 600h7410m0 600H0m0 600h7410m0 600H0"
-                        stroke="#fff"
-                        stroke-width="300"
-                    />
-                    <path fill="#3c3b6e" d="M0 0h2964v2100H0z" />
-                    <g fill="#fff">
-                        <g id="d">
-                            <g id="c">
-                                <g id="e">
-                                    <g id="b">
-                                        <path
-                                            id="a"
-                                            d="M247 90l70.534 217.082-184.66-134.164h228.253L176.466 307.082z"
-                                        />
-                                        <use xlink:href="#a" y="420" />
-                                        <use xlink:href="#a" y="840" />
-                                        <use xlink:href="#a" y="1260" />
-                                    </g>
-                                    <use xlink:href="#a" y="1680" />
-                                </g>
-                                <use xlink:href="#b" x="247" y="210" />
-                            </g>
-                            <use xlink:href="#c" x="494" />
-                        </g>
-                        <use xlink:href="#d" x="988" />
-                        <use xlink:href="#c" x="1976" />
-                        <use xlink:href="#e" x="2470" />
-                    </g>
+                    <path fill="#FFC107" d="M0 0h512v341.3H0z" />
+                    <path fill="#D81B60" d="M0 341.3h512v170.7H0z" />
                 </svg>
             </button>
             <!-- Dropdown -->
@@ -220,6 +200,7 @@ import { Link } from "@inertiajs/vue3";
                     <li>
                         <a
                             href="#"
+                            data-tooltip-target="tooltip-language-es"
                             class="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100 dark:hover:text-white dark:text-gray-300 dark:hover:bg-gray-600"
                             role="menuitem"
                         >
@@ -228,99 +209,34 @@ import { Link } from "@inertiajs/vue3";
                                     aria-hidden="true"
                                     class="h-3.5 w-3.5 rounded-full mr-2"
                                     xmlns="http://www.w3.org/2000/svg"
-                                    id="flag-icon-css-us"
-                                    viewBox="0 0 512 512"
-                                >
-                                    <g fill-rule="evenodd">
-                                        <g stroke-width="1pt">
-                                            <path
-                                                fill="#bd3d44"
-                                                d="M0 0h247v10H0zm0 20h247v10H0zm0 20h247v10H0zm0 20h247v10H0zm0 20h247v10H0zm0 20h247v10H0zm0 20h247v10H0z"
-                                                transform="scale(3.9385)"
-                                            />
-                                            <path
-                                                fill="#fff"
-                                                d="M0 10h247v10H0zm0 20h247v10H0zm0 20h247v10H0zm0 20h247v10H0zm0 20h247v10H0zm0 20h247v10H0z"
-                                                transform="scale(3.9385)"
-                                            />
-                                        </g>
-                                        <path
-                                            fill="#192f5d"
-                                            d="M0 0h98.8v70H0z"
-                                            transform="scale(3.9385)"
-                                        />
-                                        <path
-                                            fill="#fff"
-                                            d="M8.2 3l1 2.8H12L9.7 7.5l.9 2.7-2.4-1.7L6 10.2l.9-2.7-2.4-1.7h3zm16.5 0l.9 2.8h2.9l-2.4 1.7 1 2.7-2.4-1.7-2.4 1.7 1-2.7-2.4-1.7h2.9zm16.5 0l.9 2.8H45l-2.4 1.7 1 2.7-2.4-1.7-2.4 1.7 1-2.7-2.4-1.7h2.9zm16.4 0l1 2.8h2.8l-2.3 1.7.9 2.7-2.4-1.7-2.3 1.7.9-2.7-2.4-1.7h3zm16.5 0l.9 2.8h2.9l-2.4 1.7 1 2.7-2.4-1.7-2.4 1.7 1-2.7-2.4-1.7h2.9zm16.5 0l.9 2.8h2.9L92 7.5l1 2.7-2.4-1.7-2.4 1.7 1-2.7-2.4-1.7h2.9zm-74.1 7l.9 2.8h2.9l-2.4 1.7 1 2.7-2.4-1.7-2.4 1.7 1-2.7-2.4-1.7h2.9zm16.4 0l1 2.8h2.8l-2.3 1.7.9 2.7-2.4-1.7-2.3 1.7.9-2.7-2.4-1.7h3zm16.5 0l.9 2.8h2.9l-2.3 1.7.9 2.7-2.4-1.7-2.3 1.7.9-2.7-2.4-1.7h2.9zm16.5 0l.9 2.8H45l-2.4 1.7 1 2.7-2.4-1.7-2.4 1.7 1-2.7-2.4-1.7h2.9zm16.4 0l1 2.8h2.8l-2.3 1.7.9 2.7-2.4-1.7-2.3 1.7.9-2.7-2.4-1.7h3zm16.5 0l.9 2.8h2.9l-2.3 1.7.9 2.7-2.4-1.7-2.3 1.7.9-2.7-2.4-1.7h2.9zm16.5 0l.9 2.8h2.9L92 21.5l1 2.7-2.4-1.7-2.4 1.7 1-2.7-2.4-1.7h2.9zm-74.1 7l.9 2.8h2.9l-2.4 1.7 1 2.7-2.4-1.7-2.4 1.7 1-2.7-2.4-1.7h2.9zm16.4 0l1 2.8h2.8l-2.3 1.7.9 2.7-2.4-1.7-2.3 1.7.9-2.7-2.4-1.7h3zm16.5 0l.9 2.8h2.9l-2.3 1.7.9 2.7-2.4-1.7-2.3 1.7.9-2.7-2.4-1.7h2.9zm16.5 0l.9 2.8h2.9l-2.4 1.7 1 2.7-2.4-1.7-2.4 1.7 1-2.7-2.4-1.7H65zm16.4 0l1 2.8H86l-2.3 1.7.9 2.7-2.4-1.7-2.3 1.7.9-2.7-2.4-1.7h3zm-74 7l.8 2.8h3l-2.4 1.7.9 2.7-2.4-1.7L6 24.2l.9-2.7-2.4-1.7h3zm16.4 0l.9 2.8h2.9l-2.3 1.7.9 2.7-2.4-1.7-2.3 1.7.9-2.7-2.4-1.7h2.9zm16.5 0l.9 2.8H45l-2.4 1.7 1 2.7-2.4-1.7-2.4 1.7 1-2.7-2.4-1.7h2.9zm16.4 0l1 2.8h2.8l-2.3 1.7.9 2.7-2.4-1.7-2.3 1.7.9-2.7-2.4-1.7h3zm16.5 0l.9 2.8h2.9l-2.3 1.7.9 2.7-2.4-1.7-2.3 1.7.9-2.7-2.4-1.7h2.9zm16.5 0l.9 2.8h2.9L92 35.5l1 2.7-2.4-1.7-2.4 1.7 1-2.7-2.4-1.7h2.9zm-74.1 7l.9 2.8h2.9l-2.4 1.7 1 2.7-2.4-1.7-2.4 1.7 1-2.7-2.4-1.7h2.9zm16.4 0l1 2.8h2.8l-2.3 1.7.9 2.7-2.4-1.7-2.3 1.7.9-2.7-2.4-1.7h3zm16.5 0l.9 2.8h2.9l-2.3 1.7.9 2.7-2.4-1.7-2.3 1.7.9-2.7-2.4-1.7h2.9zm16.5 0l.9 2.8h2.9l-2.4 1.7 1 2.7-2.4-1.7-2.4 1.7 1-2.7-2.4-1.7H65zm16.4 0l1 2.8H86l-2.3 1.7.9 2.7-2.4-1.7-2.3 1.7.9-2.7-2.4-1.7h3zm-74 7l.8 2.8h3l-2.4 1.7.9 2.7-2.4-1.7L6 52.2l.9-2.7-2.4-1.7h3zm16.4 0l.9 2.8h2.9l-2.3 1.7.9 2.7-2.4-1.7-2.3 1.7.9-2.7-2.4-1.7h2.9zm16.5 0l.9 2.8H45l-2.4 1.7 1 2.7-2.4-1.7-2.4 1.7 1-2.7-2.4-1.7h2.9zm16.4 0l1 2.8h2.8l-2.3 1.7.9 2.7-2.4-1.7-2.3 1.7.9-2.7-2.4-1.7h3zm16.5 0l.9 2.8h2.9l-2.3 1.7.9 2.7-2.4-1.7-2.3 1.7.9-2.7-2.4-1.7h2.9zm16.5 0l.9 2.8h2.9L92 49.5l1 2.7-2.4-1.7-2.4 1.7 1-2.7-2.4-1.7h2.9z"
-                                            transform="scale(3.9385)"
-                                        />
-                                    </g>
-                                </svg>
-                                English (US)
-                            </div>
-                        </a>
-                    </li>
-                    <li>
-                        <a
-                            href="#"
-                            class="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:text-white dark:hover:bg-gray-600"
-                            role="menuitem"
-                        >
-                            <div class="inline-flex items-center">
-                                <svg
-                                    aria-hidden="true"
-                                    class="h-3.5 w-3.5 rounded-full mr-2"
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    id="flag-icon-css-de"
+                                    id="flag-icon-css-es"
                                     viewBox="0 0 512 512"
                                 >
                                     <path
-                                        fill="#ffce00"
-                                        d="M0 341.3h512V512H0z"
+                                        fill="#FFC107"
+                                        d="M0 0h512v341.3H0z"
                                     />
-                                    <path d="M0 0h512v170.7H0z" />
                                     <path
-                                        fill="#d00"
-                                        d="M0 170.7h512v170.6H0z"
+                                        fill="#D81B60"
+                                        d="M0 341.3h512v170.7H0z"
                                     />
                                 </svg>
-                                Deutsch
+                                Español
                             </div>
                         </a>
-                    </li>
-                    <li>
-                        <a
-                            href="#"
-                            class="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:text-white dark:hover:bg-gray-600"
-                            role="menuitem"
+                        <div
+                            id="tooltip-language-es"
+                            role="tooltip"
+                            class="inline-block absolute invisible z-10 py-2 px-3 text-sm font-medium text-white bg-gray-900 rounded-lg shadow-sm opacity-0 transition-opacity duration-300 tooltip"
                         >
-                            <div class="inline-flex items-center">
-                                <svg
-                                    aria-hidden="true"
-                                    class="h-3.5 w-3.5 rounded-full mr-2"
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    id="flag-icon-css-it"
-                                    viewBox="0 0 512 512"
-                                >
-                                    <g fill-rule="evenodd" stroke-width="1pt">
-                                        <path fill="#fff" d="M0 0h512v512H0z" />
-                                        <path
-                                            fill="#009246"
-                                            d="M0 0h170.7v512H0z"
-                                        />
-                                        <path
-                                            fill="#ce2b37"
-                                            d="M341.3 0H512v512H341.3z"
-                                        />
-                                    </g>
-                                </svg>
-                                Italiano
-                            </div>
-                        </a>
+                            Función no implementada
+                            <div class="tooltip-arrow" data-popper-arrow></div>
+                        </div>
                     </li>
                     <li>
                         <a
                             href="#"
+                            data-tooltip-target="tooltip-language-en"
                             class="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100 dark:hover:text-white dark:text-gray-300 dark:hover:bg-gray-600"
                             role="menuitem"
                         >
@@ -329,51 +245,78 @@ import { Link } from "@inertiajs/vue3";
                                     aria-hidden="true"
                                     class="h-3.5 w-3.5 rounded-full mr-2"
                                     xmlns="http://www.w3.org/2000/svg"
-                                    xmlns:xlink="http://www.w3.org/1999/xlink"
-                                    id="flag-icon-css-cn"
+                                    id="flag-icon-css-gb"
                                     viewBox="0 0 512 512"
                                 >
-                                    <defs>
-                                        <path
-                                            id="a"
-                                            fill="#ffde00"
-                                            d="M1-.3L-.7.8 0-1 .6.8-1-.3z"
-                                        />
-                                    </defs>
-                                    <path fill="#de2910" d="M0 0h512v512H0z" />
-                                    <use
-                                        width="30"
-                                        height="20"
-                                        transform="matrix(76.8 0 0 76.8 128 128)"
-                                        xlink:href="#a"
+                                    <path fill="#012169" d="M0 0h512v512H0z" />
+                                    <path
+                                        fill="#FFF"
+                                        d="M512 0v45.7L277.3 256 512 466.3V512H466.3L256 277.3 45.7 512H0v-45.7L234.7 256 0 45.7V0h45.7L256 234.7 466.3 0z"
                                     />
-                                    <use
-                                        width="30"
-                                        height="20"
-                                        transform="rotate(-121 142.6 -47) scale(25.5827)"
-                                        xlink:href="#a"
+                                    <path
+                                        fill="#C8102E"
+                                        d="M256 0v234.7L512 0h-45.7zM256 277.3L512 512h-45.7zM0 512l256-234.7V512H0zm256-277.3L0 0h45.7z"
                                     />
-                                    <use
-                                        width="30"
-                                        height="20"
-                                        transform="rotate(-98.1 198 -82) scale(25.6)"
-                                        xlink:href="#a"
+                                    <path
+                                        fill="#FFF"
+                                        d="M147.4 0H192v211.2h320v89.6H192V512h-44.6V300.8H0v-89.6h147.4z"
                                     />
-                                    <use
-                                        width="30"
-                                        height="20"
-                                        transform="rotate(-74 272.4 -114) scale(25.6137)"
-                                        xlink:href="#a"
-                                    />
-                                    <use
-                                        width="30"
-                                        height="20"
-                                        transform="matrix(16 -19.968 19.968 16 256 230.4)"
-                                        xlink:href="#a"
+                                    <path
+                                        fill="#C8102E"
+                                        d="M170.7 0H192v512h-21.3zM0 234.7h512v42.6H0z"
                                     />
                                 </svg>
+                                Inglés
                             </div>
                         </a>
+                        <div
+                            id="tooltip-language-en"
+                            role="tooltip"
+                            class="inline-block absolute invisible z-10 py-2 px-3 text-sm font-medium text-white bg-gray-900 rounded-lg shadow-sm opacity-0 transition-opacity duration-300 tooltip"
+                        >
+                            Función no implementada
+                            <div class="tooltip-arrow" data-popper-arrow></div>
+                        </div>
+                    </li>
+                    <li>
+                        <a
+                            href="#"
+                            data-tooltip-target="tooltip-language-fr"
+                            class="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100 dark:hover:text-white dark:text-gray-300 dark:hover:bg-gray-600"
+                            role="menuitem"
+                        >
+                            <div class="inline-flex items-center">
+                                <svg
+                                    aria-hidden="true"
+                                    class="h-3.5 w-3.5 rounded-full mr-2"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    id="flag-icon-css-fr"
+                                    viewBox="0 0 512 512"
+                                >
+                                    <path
+                                        fill="#0052B4"
+                                        d="M0 0h170.7v512H0z"
+                                    />
+                                    <path
+                                        fill="#FFF"
+                                        d="M170.7 0h170.6v512H170.7z"
+                                    />
+                                    <path
+                                        fill="#F00000"
+                                        d="M341.3 0H512v512H341.3z"
+                                    />
+                                </svg>
+                                Francés
+                            </div>
+                        </a>
+                        <div
+                            id="tooltip-language-fr"
+                            role="tooltip"
+                            class="inline-block absolute invisible z-10 py-2 px-3 text-sm font-medium text-white bg-gray-900 rounded-lg shadow-sm opacity-0 transition-opacity duration-300 tooltip"
+                        >
+                            Función no implementada
+                            <div class="tooltip-arrow" data-popper-arrow></div>
+                        </div>
                     </li>
                 </ul>
             </div>
